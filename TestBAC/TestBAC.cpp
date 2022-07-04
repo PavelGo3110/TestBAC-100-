5362a5687278d5c6d01f933950d34f169ff5dfc7
Pavel Goishan <pavel.goishan@incredibuild.com>
2022-07-04 09:31:53 +0000

    Change2

--- TestBAC/TestBAC.cpp
+++ TestBAC/TestBAC.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "This is TestBAC project5 \n";
+    std::cout << "This is TestBAC project2 \n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
